34d136992de22cd9b736ff1eb97590cf23bbfaa2
Jaryd Krishnan <jaryd@hey.com>
2023-10-09 11:47:07 -0500

    Authenticate against the admin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  ActiveAdmin.routes(self)
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  authenticate :user do
+    ActiveAdmin.routes(self)
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
